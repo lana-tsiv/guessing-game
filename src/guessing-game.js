e311d283f681ea1cc9f4531e575448876a3bb425
Lana <tiushkievich00@mail.ru>
2022-08-15 20:03:11 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.number = Math.ceil((this.max - this.min) / 2) + this.min;
+      return this.number;
     }
-
+  
     lower() {
-
+      this.setRange(this.min, this.number);
     }
-
+  
     greater() {
-
+      this.setRange(this.number, this.max);
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
